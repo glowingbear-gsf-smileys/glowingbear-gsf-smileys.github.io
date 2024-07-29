9c207f0a71a3ffd9f98c889660821c0dd31fd6f9
Matthew Moore <matthewemmoore@googlemail.com>
2024-07-29 08:55:14 +0100

    Add main back

---
+++ main.js
@@ -0,0 +1,43 @@
+
+"use strict";
+
+// import "angular";
+// import "angular-route";
+// import "angular-sanitize";
+// import "angular-touch";
+
+import "./3rdparty/angular.min.js";
+import "./3rdparty/angular-route.min.js";
+import "./3rdparty/angular-sanitize.min.js";
+import "./3rdparty/angular-touch.min.js";
+
+import "./3rdparty/underscore-min.js";
+import "./3rdparty/inflate.min.js";
+
+import "./js/localstorage.js";
+import "./js/weechat.js";
+import "./js/irc-utils.js";
+import "./js/glowingbear.js";
+import "./js/settings.js";
+import "./js/utils.js";
+import "./js/notifications.js";
+import "./js/filters.js";
+import "./js/handlers.js";
+import "./js/connection.js"
+import "./js/file-change.js"
+import "./js/imgur-drop-directive.js";
+import "./js/whenscrolled-directive.js";
+import "./js/inputbar.js";
+import "./js/plugin-directive.js";
+import "./js/websockets.js";
+import "./js/bufferResume.js";
+import "./js/models.js";
+import "./js/plugins.js";
+import "./js/imgur.js";
+import "./js/aho_corasick.js";
+import "./smileys/directory.json"
+import "./js/file-change.js";
+import "./3rdparty/favico-0.3.10.min.js";
+
+
+
