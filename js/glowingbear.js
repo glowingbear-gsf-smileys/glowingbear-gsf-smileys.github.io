014a9b9961994393730c3cad379fc69294ed3140
Matthew Moore <matthew.moore@answerdigital.com>
2024-07-30 14:46:52 +0100

    Upgrade to glowingbear 0.9.0

--- js/glowingbear.js
+++ js/glowingbear.js
@@ -59,7 +59,6 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
         'fontsize': '14px',
         'fontfamily': (utils.isMobileUi() ? 'sans-serif' : 'Inconsolata, Consolas, Monaco, Ubuntu Mono, monospace'),
         'readlineBindings': false,
-        'enableJSEmoji': !utils.isMobileUi(),
         'enableMathjax': false,
         'enableQuickKeys': true,
         'customCSS': '',
@@ -79,7 +78,6 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     $rootScope.countWatchers = function () {
         $log.debug($rootScope.$$watchersCount);
     };
-
 
     // Detect page visibility attributes
     (function() {
@@ -119,13 +117,6 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     $scope.show_tls_warning = (["https:", "file:"].indexOf(window.location.protocol) === -1) &&
         (["localhost", "127.0.0.1", "::1"].indexOf(window.location.hostname) === -1) &&
         !window.is_electron && !utils.isCordova();
-
-    if (window.is_electron) {
-        // Use packaged emojione sprite in the electron app
-        emojione.imageType = 'svg';
-        emojione.sprites = true;
-        emojione.imagePathSVGSprites = './3rdparty/emojione.sprites.svg';
-    }
 
     $rootScope.isWindowFocused = function() {
         if (typeof $scope.documentHidden === "undefined") {
@@ -730,7 +721,7 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     };
 
     $scope.connect = function() {
-
+        document.getElementById('audioNotificationInitializer').play(); // Plays some silence, this will enable autoplay for notifications
         notifications.requestNotificationPermission();
         $rootScope.sslError = false;
         $rootScope.securityError = false;
@@ -739,7 +730,7 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
         $scope.connectbutton = 'Connecting';
         $scope.connectbuttonicon = 'glyphicon-refresh glyphicon-spin';
         connection.connect(settings.host, settings.port, settings.path, $scope.password, settings.ssl, settings.useTotp, $scope.totp);
-        $scope.totp = "";//clear for next time
+        $scope.totp = ""; // Clear for next time
     };
 
     $scope.disconnect = function() {
